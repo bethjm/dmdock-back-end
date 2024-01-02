adaa43c26afd7441672fa27da59e29eef849100a
bethjm <b.jeanne.mills@gmail.com>
2024-01-02 09:49:31 -0500

    Configure STATIC_ROOT and update middleware for Whitenoise

--- dmdock_project/settings.py
+++ dmdock_project/settings.py
@@ -88,6 +88,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
