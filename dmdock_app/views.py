0b451a05186e5d7e21dd6a847ad4d91155353d04
bethjm <b.jeanne.mills@gmail.com>
2024-01-02 14:41:40 -0500

    make it better

--- dmdock_app/views.py
+++ dmdock_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
