b356de5761367149f9e234ff29c6a659e3023441
bethjm <b.jeanne.mills@gmail.com>
2024-01-02 20:09:51 -0500

    make it better

--- dmdock_project/settings.py
+++ dmdock_project/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+from django.utils import timezone
+
+now = timezone.now
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
